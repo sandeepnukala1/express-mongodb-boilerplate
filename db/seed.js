439e60a7be4e6ee4bef0471f4575d4a636b4fbc7
Sandeep <sandeepn@bu.edu>
2021-03-20 12:01:10 -0400

    boilerplate for express and mongo

---
+++ db/seed.js
@@ -0,0 +1,18 @@
+//Import The Database Connection
+const mongoose = require("./connection")
+
+///////////////////////////////////////////
+// IMPORT YOUR MODELS BELOW
+///////////////////////////////////////////
+
+///////////////////////////////////////////
+// DO YOUR DATABASE OPERATIONS IN BELOW FUNCTION
+///////////////////////////////////////////
+
+const seed = async () => {
+  //*********Code Goes Here
+  //***************************** */
+}
+
+// Run Seed Function
+seed()
